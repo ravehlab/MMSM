aff5f49c377b935975cceda9222b1e4b6a7cb30e
KessemClein <kessem.clein@mail.huji.ac.il>
2021-01-27 10:27:36 +0200

    K_CentersCoarseGrain class - final preprocessing stage before MSM estimation.

--- prototype/coarse_grain.py
+++ prototype/coarse_grain.py
@@ -2,7 +2,67 @@ from HMSM.util.clustering import k_centers, extend_k_centers
 
 class K_CentersCoarseGrain:
 
-    def __init__(self):
+    def __init__(self, cutoff, representative_sample_size=10):
         self.nearest_neighbors = None
         self.centers = None
+        self._k_centers_initiated = False
+
+        self.representative_sample_size = representative_sample_size
         self.representatives = dict()
+        
+        self.cluster_count = util.count_dict()
+        self.cutoff = cutoff
+    
+    @property
+    def n_clusters(self):
+        return len(self.centers)
+    
+
+    def get_coarse_grained_clusters(self, data : np.ndarray):
+        if self._k_centers_initiated:
+            self.nn, clusters, self.centers = extend_k_centers(data, self.nn, self.centers, self.cutoff)
+        else:
+            self.nn, clusters, self.centers = k_centers(data, cutoff=self.cutoff)
+            self._k_centers_initiated = True
+
+        if np.max(clusters) > self.n_clusters-1:
+            new_clusters = np.unique(clusters[np.where(clusters>self.n_clusters-1)])
+            self._add_clusters(new_clusters)
+
+        self._sample_representatives(clusters, data)
+
+        return self._get_ids(clusters)
+
+    def sample_from(self, microstate_id):
+        random_index = np.random.randint(len(self.representatives[microstate_id]))
+        return self.representatives[microstate_id][random_index]
+
+
+    def _get_ids(self, cluster_indices):
+        return [self.cluster_inx_2_id[inx] for inx in cluster_indices]
+
+    def _add_clusters(self, new_clusters):
+        for cluster in new_clusters:
+            self.cluster_inx_2_id[cluster] = util.get_unique_id()
+
+    def _sample_representatives(self, clusters, data):
+        """
+        Keep representative samples for each cluster, such that the probability of a representative
+        x from cluster j being sampled (when sampling from the representatives) is 1/n, where n is 
+        the number of points x' that have been observed in j.
+        """
+        for i, cluster_index in enumerate(clusters):
+            self.cluster_count[cluster_index] += 1
+            cluster_id = self.cluster_inx_2_id[cluster_index]
+            if not self.representatives.get(cluster_id):
+                # If this is the first observation of this cluster
+                self.representatives[cluster_id] = [data[i]]
+            elif len(self.representatives[cluster_id] ) < self.representative_sample_size:
+                self.representatives[cluster_id].append(data[i])
+            elif np.random.random() <= (1/self.cluster_count[cluster_index]):
+                # with probability 1/n, keep this point. It can be proven easily by induction
+                # that this gives the desired property.
+                random_index = np.random.randint(self.representative_sample_size)
+                self.representatives[cluster_id][random_index] = data[i]
+
+
