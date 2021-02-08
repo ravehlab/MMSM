b5c40f8c787fac3bfbfa3cb92fa5070ab2b536de
KessemClein <kessem.clein@mail.huji.ac.il>
2021-02-08 11:17:48 +0200

    imports

--- prototype/__init__.py
+++ prototype/__init__.py
@@ -1,10 +1,11 @@
 
-from . import hierarchical_markov_tree
+from . import hierarchical_msm_tree
+from . import hierarchical_msm_estimator
 from . import coarse_grain
 from . import sampler
 from . import hmsm_config
 
-from .hierarchical_markov_tree import *
+from .hierarchical_msm_estimator import *
 from .coarse_grain import *
 from .sampler import *
 from .hmsm_config import *
