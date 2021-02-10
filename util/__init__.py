533cea314d06496dfc15b932482c5c57f3197f13
KessemClein <kessem.clein@mail.huji.ac.il>
2021-02-10 10:51:00 +0200

    UniquePriorityQueue - exactly what it sounds like.

--- util/__init__.py
+++ util/__init__.py
@@ -1,2 +1,3 @@
 from . import clustering
 from . import linalg
+from .unique_priority_queue import UniquePriorityQueue
---
+++ util/unique_priority_queue.py
@@ -0,0 +1,45 @@
+from queue import PriorityQueue
+
+class UniquePriorityQueue:
+    """
+    UniquePriorityQueue
+
+    A class for a simple priority queue, with no duplicate values (even if they have different
+    priorities).
+    """
+
+
+    def __init__(self):
+        self._queue = PriorityQueue
+        self._items = set()
+
+    @property
+    def not_empty(self):
+        """
+        True iff the queue is not empty
+        """
+        return len(self._items) > 0
+
+    def put(self, item):
+        """
+        Add an item to the queue. If the item is already in the queue, will not be added.
+
+        Parameters
+        ----------
+        item : tuple
+            A tuple (priority, value)
+        """
+        if item[1] in self._items:
+            return
+        self._items.add(item[1])
+        self._queue.put(item)
+
+    def get(self):
+        """
+        Get a value from the queue with minimal priority. If the queue is empty returns None.
+        """
+        if self.not_empty:
+            _, item = self._queue.get()
+            self._items.remove(item)
+            return item
+        return None
