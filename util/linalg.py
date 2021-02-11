0d954ef46503b93461b2f49a0a33f8aecc2ab671
KessemClein <kessem.clein@mail.huji.ac.il>
2021-02-11 18:07:33 +0200

    removed irrelevant stuff from linalg

--- util/linalg.py
+++ util/linalg.py
@@ -1,9 +1,9 @@
 from functools import reduce
+import warnings
 import numpy as np
 import matplotlib as mpl
 from numpy.linalg import LinAlgError
 from sklearn.cluster import MiniBatchKMeans
-import warnings; warnings.simplefilter('default')
 
 sample_from_ndarray = lambda array, n : array[np.random.choice(array.shape[0], n, replace=False)] \
                                         if n<=array.shape[0] else array.copy()
@@ -44,79 +44,27 @@ def stationary_distribution(transition_matrix, max_iter=1e2) -> np.ndarray:
         z = np.matmul(z,z)
         if np.all(np.isclose(z[0], z)):
             return (z[0])
-    raise UserWarning("Graph is not ergodic; The distribution returned assumes uniform distribution over starting vertices.")
+    warnings.warn("Graph is not ergodic; The distribution returned assumes uniform \
+                   distribution over starting vertices.")
     return np.mean(z, axis=0)
 
-    
-
-def distance_matrix(X, Y=None, self_distance=np.inf):
-    """distance_matrix.
-    Calculate a pointwise euclidean distance matrix between X and Y. If Y is None returns
-    the pairwise distances between the points in X.
-    """
-    if Y is None:
-        dist = np.array([np.linalg.norm(X-X[j], axis=1) for j in range(X.shape[0])])
-        np.fill_diagonal(dist, self_distance)
-        return dist
-
-    if not isinstance(Y, np.ndarray):
-        y = np.array(Y)
-    if Y.ndim==1:
-        if X.shape[-1] != Y.shape[-1]:
-            raise LinAlgError(f"X and Y last dimensions don't match: {X.shape}, {Y.shape}")
-        dist = np.linalg.norm(X-Y, axis=1)
-    else:
-        if X.shape[-1] != Y.shape[-1]:
-            raise LinAlgError(f"X and Y last dimensions don't match: {X.shape}, {Y.shape}")
-        dist = np.array([np.linalg.norm(X-Y[j], axis=1) for j in range(Y.shape[0])])
-    _assert_2d(dist)
-    return dist
-
-def min_connected_graph(distances):
-    _assert_2d(distances)
-    if np.any(distances<0):
-        raise LinAlgError("Negative distances not supported")
-
-    k = distances.shape[0]
-    connectivity = np.eye(k)
-    # first connect all the nearest neighbors
-    for j in range(k):
-        nearest_neighbor = np.argmin(distances[j])
-        connectivity[j, nearest_neighbor] = 1
-        connectivity[nearest_neighbor, j] = 1
-    cc = connected_components(connectivity)
-    # now connect the seperate components by their minimal edges
-    while cc is not True:
-        # find the shortest edge from the first cc to a vertex outside it
-        first_component = np.where(cc[0])[0]
-        all_the_rest = np.where(1-cc[0])[0]
-        inter_component_distances = distances[np.ix_(first_component, all_the_rest)]
-        shortest_distance = np.argmin(inter_component_distances)
-        u,v = np.unravel_index(shortest_distance, inter_component_distances.shape) # numpy index stuff
-        u = first_component[u] # get the absolute index of u 
-        v = all_the_rest[v] # get the absolute index of u 
-        # now connect u and v
-        connectivity[u,v] = 1
-        connectivity[v,u] = 1
-        cc = connected_components(connectivity)
-    return connectivity
-
-def connected_components(E):
-    
-    #E should be a connectivity graph of an aperiodic directed graph.
-    # returns true if the graph is strongly connected, otherwise returns cc an ndarray of bool with
-    # shape (n,k) where n is the number of connected components, and k is the number of vertices, and 
-    # cc[i,j]==True if and only if j is in the i'th component
-    _assert_2d(E)
-    k = E.shape[0]
-    cc = np.abs(np.linalg.matrix_power(E, k))
-    cc = np.unique(cc>0, axis=0)
-    if np.all(cc):
-        return True
-    else:
-        return cc
-
 def normalize_rows(matrix:np.ndarray, norm:int=2)->np.ndarray:
+    """
+    Normalize a matrix across rows (axis 1).
+
+    Parameters
+    ----------
+    matrix : np.ndarray
+        The matrix to normalize
+    norm : int
+        Which norm to normalize by. Default is 2 (Euclidean norm).
+
+    Returns
+    -------
+    normalized_matrix : np.ndarray
+        A copy of the matrix, normalized across the rows.
+
+    """
     _assert_finite(matrix)
     normalized = matrix/np.linalg.norm(matrix, norm, axis=1)[:, None]
     return np.nan_to_num(normalized, posinf=1.)
@@ -127,6 +75,7 @@ def normalized_laplacian(P):
     D = np.diag(np.power(np.sum(P, axis=1), -0.5)) #D^-1/2
     L = np.eye(N) - reduce(np.matmul, [D, P, D])
     return L
+
 
 def get_longest_timescale(P, tau):
     """Get the implied timescale of the second largest eigenvector of a transition matrix P with 
@@ -139,7 +88,7 @@ def get_longest_timescale(P, tau):
     else:
         two_largest = np.argpartition(-w, 2)[:2]
         lambda_2 = w[two_largest][1]
-        if np.isclose(np.log(lambda_2), 0):
+        if np.isclose(lambda_2, 1):
             return np.inf
 
     t_2 = np.abs(tau/np.log(lambda_2))
