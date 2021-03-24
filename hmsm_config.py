0e757c9400cd9e745497b3c6003a367a2063c235
KessemClein <kessem.clein@mail.huji.ac.il>
2021-03-24 16:14:07 +0200

    HMSMConfig update

--- hmsm_config.py
+++ hmsm_config.py
@@ -2,7 +2,7 @@
 
 # Author: Kessem Clein <kessem.clein@mail.huji.ac.il>
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 @dataclass
 class HMSMConfig:
@@ -18,26 +18,22 @@ class HMSMConfig:
     n_microstates: int=20
     n_samples: int=5
     sample_len: int=5
-    split_condition: str='auto'
-    split_method: str='auto'
-    sample_method: str='auto'
     parent_update_condition: str='auto'
     parent_update_threshold: float=0.1
-    sampling_optimizer_type: str='auto'
-    partition_estimator_type: str='auto'
+    sampling_optimizer: str='auto'
+    sampling_optimizer_kwargs: dict=field(default_factory=dict)
     partition_estimator: str='auto'
+    partition_kwargs: dict=field(default_factory=dict)
     transition_estimator: str='Dirichlet_MMSE'
     """
     base_tau: int=1
     n_microstates: int=20
     n_samples: int=5
     sample_len: int=5
-    split_condition: str='auto'
-    split_method: str='auto'
-    sample_method: str='auto'
     parent_update_condition: str='auto'
     parent_update_threshold: float=0.1
-    sampling_optimizer_type: str='auto'
-    partition_estimator_type: str='auto'
+    sampling_optimizer: str='auto'
+    sampling_optimizer_kwargs: dict=field(default_factory=dict)
     partition_estimator: str='auto'
+    partition_kwargs: dict=field(default_factory=dict)
     transition_estimator: str='Dirichlet_MMSE'
