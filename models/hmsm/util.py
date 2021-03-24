082763a203b5749c873d8623e969de73d17ed552
KessemClein <kessem.clein@mail.huji.ac.il>
2021-03-24 16:12:50 +0200

    moved util methods to more logical places

---
+++ models/hmsm/util.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+def get_parent_update_condition(condition, threshold=0.1):
+    if condition in ("auto", 'fractional_difference'):
+        return lambda vertex : max_fractional_difference(vertex._last_update_sent,
+                                                         vertex.get_external_T()) >= threshold
+    raise NotImplementedError(f"Parent update condition {condition} not implemented.")
+
+
+def max_fractional_difference(ext_T1, ext_T2):
+    """
+    Get the maximal change in transition probabilities between ext_T1 and ext_T2, where ext_T1/2
+    are tuples of (ids, transition_probabilities), as returned by
+    HierarchicalMSMVertex.get_external_T.
+
+    Parameters
+    ----------
+    ext_T1 : tuple
+        the previous value of external_T
+    ext_T2 : tuple
+        the current value of external_T
+
+    Returns
+    -------
+    the maximal difference in transition probabilities, as a fractional difference, that is:
+    max[abs(1-(p1/p2)) for p1 in ext_T1 and p2 in ext_T2 with matching ids]
+    """
+    ids1, T1 = ext_T1
+    ids2, T2 = ext_T2
+    assert T1.shape == T2.shape, "external_T has changed shape since last update"
+    sort1 = np.argsort(ids1) # sort by ids
+    sort2 = np.argsort(ids2) # sort by ids
+    max_diff = 0
+
+    for i in range(len(sort1)):
+        p1 = T1[sort1[i]]
+        p2 = T2[sort2[i]]
+        diff = np.abs(1-(p1/p2))
+        max_diff = max(max_diff, diff)
+
+    return max_diff
--- util/util.py
+++ util/util.py
@@ -5,13 +5,11 @@
 import time
 import sys
 from collections import defaultdict
-from uuid import uuid4
 import numpy as np
-from HMSM.util import clustering
 
 def count_dict(depth=1) -> defaultdict:
     """
-    Gets a defaultdict, that the default value is 0. 
+    Gets a defaultdict, that the default value is 0.
     If the parameter depth is >1, this will be a defaultdict of such defaultdicts with depth depth.
     For example, count_dict(2)
 
@@ -52,39 +50,6 @@ def count_dict(depth=1) -> defaultdict:
 
 get_unique_id = lambda : np.random.choice(sys.maxsize)
 
-def max_fractional_difference(ext_T1, ext_T2):
-    """
-    Get the maximal change in transition probabilities between ext_T1 and ext_T2, where ext_T1/2
-    are tuples of (ids, transition_probabilities), as returned by 
-    HierarchicalMSMVertex.get_external_T.
-
-    Parameters
-    ----------
-    ext_T1 : tuple
-        the previous value of external_T
-    ext_T2 : tuple
-        the current value of external_T
-
-    Returns
-    -------
-    the maximal difference in transition probabilities, as a fractional difference, that is:
-    max[abs(1-(p1/p2)) for p1 in ext_T1 and p2 in ext_T2 with matching ids]
-    """
-    # MOVE: models.util
-    ids1, T1 = ext_T1
-    ids2, T2 = ext_T2
-    assert T1.shape == T2.shape, "external_T has changed shape since last update"
-    sort1 = np.argsort(ids1) # sort by ids
-    sort2 = np.argsort(ids2) # sort by ids
-    max_diff = 0
-
-    for i in range(len(sort1)):
-        p1 = T1[sort1[i]]
-        p2 = T2[sort2[i]]
-        diff = np.abs(1-(p1/p2))
-        max_diff = max(max_diff, diff)
-
-    return max_diff
 
 def get_threshold_check_function(greater_than : dict={}, less_than : dict={}, max_time=None):
     """
@@ -143,7 +108,7 @@ def get_threshold_check_function(greater_than : dict={}, less_than : dict={}, ma
     >>> check()
     True
     """
-    gt = defaultdict(lambda : np.inf) 
+    gt = defaultdict(lambda : np.inf)
     gt.update(greater_than) # val > gt[key] will be avaluated to False if key is not in greater_than
     lt = defaultdict(lambda : - np.inf)
     lt.update(less_than) # val < lt[key] will be avaluated to False if key is not in less_than
@@ -158,104 +123,3 @@ def get_threshold_check_function(greater_than : dict={}, less_than : dict={}, ma
                 return time.time() > start_time + max_time
         return False
     return _check_function
-
-
-### Default functions for HierarchicalMSM config dictionary. ###
-def two_fold_gibbs_split(hmsm):
-    """Get a partition of a vertex, assuming the parent's time resolution tau is twice that of
-    this vertex.
-    """
-    # MOVE: estimators
-    n = hmsm.n
-    T = hmsm.T[:n, :n]
-    tau = hmsm.tau
-    #TODO max_k should depend on number of eigenvalues with timescale greater than 2*Tau
-    max_k = min(2*int(np.sqrt(hmsm.n)), n-1) 
-    partition = clustering.gibbs_metastable_clustering(T, 2*tau, max_k)
-    taus = [tau]*len(partition)
-    return partition, taus
-
-def get_size_or_timescale_split_condition(max_size=2048, max_timescale=64):
-    """Get a function which checks if a vertices size or timescale are larger than some constant,
-    provided as arguments to this function.
-    """
-    # MOVE: estimators
-    def _split_condition(hmsm):
-        #TODO max_timescale should depend on parents timescale
-        return hmsm.timescale/hmsm.tau > max_timescale or hmsm.n > max_size
-    return _split_condition
-
-def uniform_sample(hmsm):
-    """Samples one of this vertices children uniformly.
-    """
-    # MOVE: optimizers
-    uniformly_chosen_child = np.random.choice(hmsm.n)
-    return hmsm.children[uniformly_chosen_child]
-
-def uncertainty_minimization(hmsm):
-    # MOVE: optimizers
-    children = hmsm.children
-    eps = 1e-12
-    p = np.ndarray(hmsm.n)
-    for i, child in enumerate(children):
-        p[i] = max(np.exp(-hmsm.tree.get_n_samples(child)/2), eps)
-    p = p/np.sum(p)
-    p = np.nan_to_num(p)
-    return np.random.choice(children, p=p)
-
-
-
-def get_default_config(**kwargs):
-    """get_default_config.
-    Get a config dictionary for HierarchicalMSM, with default values for all those not given as
-    arguments.
-
-    Parameters
-    ----------
-    **config_kwargs: optional
-        Any parameters for the model. Here is a list of parameters used by the model:
-
-        n_microstates: int
-            How many microstates to sample from in a single batch of samples
-        n_samples: int
-            How many samples to take from each microstate in a single batch of samples
-        sample_len: int
-            The length of each sample in a single batch of samples
-        base_tau: int
-            The number of sampler timesteps to skip between each sample used to estimate the HMSM.
-            For example if base_tau is 5, and the sampler creates a trajectory (x0, ..., x15), then
-            the sub-sampled trajectory (x0, x5, x10, x15) will be used to estimate transition rates
-            of the HMSM.
-        split_condition: callable
-            A function that takes an HierarchicalMSMVertex and returns True if this vertex should
-            split into two or more vertices
-        split_method: callable
-            A function that takes an HierarchicalMSMVertex v, and returns a tuple
-            (partition, taus), where partition is a partition of the vertices of v, and taus is
-            a list of the tau of the MSMs representing the partition parts respectively.
-        sample_method: callable
-            A function that takes an HierarchicalMSMVertex, and returns the id of one of its
-            children in the tree.
-        parent_update_threshold: float
-            The minimum fraction of a change in a transition probability between vertices that will
-            trigger the parent of the vertices to update its transition matrix.
-
-    Returns
-    -------
-    config : dict
-        Dictionary of of model parameters.
-    """
-    # MOVE: models.util
-    config = {
-              "n_microstates" : 20,
-              "n_samples" : 5,
-              "sample_len" : 5,
-              "base_tau" : 2,
-              "split_condition" : get_size_or_timescale_split_condition(),
-              "split_method" : two_fold_gibbs_split,
-              "sample_method" : uncertainty_minimization,
-              "parent_update_threshold" : 0.1,
-              "alpha" : 1
-             }
-    config.update(kwargs)
-    return config
